f35b495f599968c7eaedd289c26ed4e818bc5068
Teresa Rico <teresa.rico@iest.edu.mx>
2023-02-15 16:29:02 -0600

    Finished activity

--- Botones/app/src/main/java/com/example/botones/MainActivity2.kt
+++ Botones/app/src/main/java/com/example/botones/MainActivity2.kt
@@ -4,9 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity2 : AppCompatActivity() {
+    private var cambioIcono : Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
@@ -17,11 +20,24 @@ class MainActivity2 : AppCompatActivity() {
         Log.d("Datos", "Datos recibidos con éxito fueron frase $fraseRecibida y $edad")
 
         var btnAbrirPantalla = findViewById<Button>(R.id.btnPantalla)
+        var ivLogo = findViewById<ImageView>(R.id.ivLogo)
+        ivLogo.setOnClickListener{
+            var arregloImagenes = arrayOf<Int>(R.drawable.anahuac, R.drawable.iest)
+            if (cambioIcono) {
+                ivLogo.setImageResource(arregloImagenes[0])
+            }else{
+                ivLogo.setImageResource(arregloImagenes[1])
+            }
+            cambioIcono = !cambioIcono
+        }
+
         btnAbrirPantalla.setOnClickListener{
             val i = Intent(this, MainActivity::class.java)
             startActivity(i)
+            finish()
         }
         var btnCerrar = findViewById<Button>(R.id.btnCerrar)
         btnCerrar.setOnClickListener{finish();}
     }
+
 }
