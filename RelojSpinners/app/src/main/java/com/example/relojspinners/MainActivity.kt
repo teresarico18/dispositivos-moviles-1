192b71163e6c5ade170b1ab4bfdc5e42de929b4c
Teresa Rico <teresa.rico@iest.edu.mx>
2023-02-15 16:29:03 -0600

    Update spinner Listener, Change layout

--- RelojSpinners/app/src/main/java/com/example/relojspinners/MainActivity.kt
+++ RelojSpinners/app/src/main/java/com/example/relojspinners/MainActivity.kt
@@ -16,34 +16,52 @@ class MainActivity : Activity(), AdapterView.OnItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
     private var numSeleccionado1: String? = null
     private var numSeleccionado2: String? = null
-    lateinit var spNum1: Spinner
-    lateinit var spNum2: Spinner
+    private var numMayor: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.bnCalcular.text = "Calcular"
-        val adaptador = ArrayAdapter.createFromResource(this, R.array.numeros, android.R.layout.simple_spinner_item)
-        adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        val adaptadorsp1 = ArrayAdapter.createFromResource(this, R.array.numeros, android.R.layout.simple_spinner_item)
+        adaptadorsp1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
-        spNum1 = findViewById(R.id.sp_num1)
-        numSeleccionado1 = spNum1.getSelectedItem().toString();
-        spNum2 = findViewById(R.id.sp_num2)
-        numSeleccionado2 = spNum2.getSelectedItem().toString();
+        binding.spNum1.adapter = adaptadorsp1
+        binding.spNum1.onItemSelectedListener = this
+
+        val adaptadorsp2 = ArrayAdapter.createFromResource(this, R.array.numeros, android.R.layout.simple_spinner_item)
+        adaptadorsp2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        binding.spNum2.adapter = adaptadorsp2
+        binding.spNum2.onItemSelectedListener = this
 
         binding.bnCalcular.setOnClickListener{
             val alerta = AlertDialog.Builder(this)
+            if (Integer.valueOf(numSeleccionado1!!) >= Integer.valueOf(numSeleccionado2!!)) {
+                numMayor = numSeleccionado1
+            } else if (Integer.valueOf(numSeleccionado2!!) > Integer.valueOf(numSeleccionado1!!)) {
+                numMayor = numSeleccionado2
+            }
             alerta.setTitle("Calcular")
-                .setMessage("Miau, $numSeleccionado1, $numSeleccionado2")
+                .setMessage("El numero mayor es: $numMayor")
                 .setCancelable(false)
                 .setPositiveButton("Ok", DialogInterface.OnClickListener(){
-                    dialogInterface, i ->   Toast.makeText(this, "Una lastima:(", Toast.LENGTH_LONG).show()
+                    dialogInterface, i ->   Toast.makeText(this, "Gracias por participar", Toast.LENGTH_LONG).show()
                 })
                 .show()
 
         }
     }
 
+    override fun onItemSelected(vistaPadre: AdapterView<*>?, vistaRow: View?, posicion: Int, idVista: Long) {
+        if (vistaPadre!!.id == R.id.spNum1) {
+            numSeleccionado1 = vistaPadre.getItemAtPosition(posicion).toString()
+        } else if (vistaPadre!!.id == R.id.spNum2) {
+            numSeleccionado2 = vistaPadre.getItemAtPosition(posicion).toString()
+        }
+    }
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        //TODO("Not yet implemented")
+    }
 
 }
