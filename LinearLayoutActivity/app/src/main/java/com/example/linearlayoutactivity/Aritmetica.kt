99f3f35d15b1eed8e467a8bd56c0e7145824250d
Teresa Rico <teresa.rico@iest.edu.mx>
2023-02-15 16:29:02 -0600

    Finished activity

---
+++ LinearLayoutActivity/app/src/main/java/com/example/linearlayoutactivity/Aritmetica.kt
@@ -0,0 +1,17 @@
+package com.example.linearlayoutactivity
+
+class Aritmetica {
+    fun suma(numeroUno: Float, numeroDos: Float) : Float {
+        return numeroUno + numeroDos
+    }
+    fun resta(numeroUno: Float, numeroDos: Float) : Float {
+        return numeroUno - numeroDos
+    }
+    fun multiplicacion(numeroUno: Float, numeroDos: Float) : Float {
+        return numeroUno * numeroDos
+    }
+    fun division(numeroUno: Float, numeroDos: Float) : Float {
+        return numeroUno / numeroDos
+    }
+
+}
---
+++ LinearLayoutActivity/app/src/main/java/com/example/linearlayoutactivity/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.linearlayoutactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private var bnSuma : Button? = null
+    private var bnResta : Button? = null
+    private var etNumeroPrimero : EditText? = null
+    private var etNumeroSegundo : EditText? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        inicializarVistas()
+        asignarEventos()
+    }
+
+    private fun inicializarVistas(){
+        bnSuma = findViewById(R.id.bnSuma)
+        bnResta = findViewById(R.id.bnResta)
+        etNumeroPrimero = findViewById(R.id.etNumeroPrimero)
+        etNumeroSegundo = findViewById(R.id.etNumeroSegundo)
+    }
+
+    private fun asignarEventos(){
+        bnSuma?.setOnClickListener(this)
+        bnResta?.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        val numeroUno = etNumeroPrimero?.text.toString().toFloatOrNull()
+        val numeroDos = etNumeroSegundo?.text.toString().toFloatOrNull()
+        val aritmetica = Aritmetica()
+        when(p0?.id){
+            R.id.bnSuma -> {
+                if (numeroUno != null && numeroDos != null) {
+                    val suma = aritmetica.suma(numeroUno, numeroDos)
+                    val mensaje = Mensajes("La sumatoria fue $suma", this)
+                    mensaje.mostrarToast()
+                }
+            }
+            R.id.bnResta -> {
+                if (numeroUno != null && numeroDos != null) {
+                    val resta = aritmetica.resta(numeroUno, numeroDos)
+                    val mensaje = Mensajes("La resta fue $resta", this)
+                    mensaje.mostrarSnackbar(p0)
+                }
+            }
+
+        }
+    }
+
+
+}
