1b6d919b3ab20f12eb2303442e83a5a5d1e77a88
Teresa Rico <teresa.rico@iest.edu.mx>
2023-02-15 19:56:12 -0600

    Fist commmit

---
+++ tarea_botones_intents/app/src/main/java/com/example/tarea_botones_intents/MainActivity3.java
@@ -0,0 +1,14 @@
+package com.example.tarea_botones_intents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
